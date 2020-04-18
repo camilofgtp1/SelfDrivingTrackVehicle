b2d0c86b05f7ad6abf13236f268887b83ed9835a
Camilo Fernandez <cf@eeloy.com>
2020-04-18 16:56:49 +0200

    - refactored Q Class -

--- src/Kettenfahrzeug/Vehicle.h
+++ src/Kettenfahrzeug/Vehicle.h
@@ -28,29 +28,6 @@ class Vehicle {
 
   private:
     NewPing sLeft, sMid, sRight;
-    /*
-        const int MOTOR_RIGHT_ENABLE = 6;
-        const int MOTOR_RIGHT_INPUT1 = 7;
-        const int MOTOR_RIGHT_INPUT2 = 8;
-
-        const int MOTOR_LEFT_ENABLE = 11;
-        const int MOTOR_LEFT_INPUT1 = 12;
-        const int MOTOR_LEFT_INPUT2 = 13;
-
-        const int MAX_DISTANCE = 200;
-
-        const int TRIGGER_LEFT = 10;
-        const int ECHO_LEFT = 9;
-        const int TRIGGER_MID = 5;
-        const int ECHO_MID = 4;
-        const int TRIGGER_RIGHT = 3;
-        const int ECHO_RIGHT = 2;
-
-      replaced with define on cpp file
-
-        const int vehicleSpeed = 125;
-
-    */
 };
 
 #endif
